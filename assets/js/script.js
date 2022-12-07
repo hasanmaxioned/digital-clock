f7bbd7e4053dae87139a5a40cbe7d5828136799a
hasanmaxioned <hasanm@axioned.com>
2022-12-07 16:16:39 +0530

    js complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,26 @@
-/* Author: 
+setInterval(showTime, 1000);
+function showTime() {
+  let time = new Date();
+  let hour = time.getHours();
+  let mint = time.getMinutes();
+  let sec = time.getSeconds();
+  let am_pm = "AM";
 
-*/
+  if(hour > 12) {
+    hour = hour - 12;
+    am_pm = "PM";
+  }
+  if(hour == 12) {
+    hr = 12;
+    am_pm = "AM";
+  }
 
+  hour = hour < 10 ? "0" + hour : hour;
+  mint = mint < 10 ? "0" + mint : mint;
+  sec = sec < 10 ? "0" + sec : sec;
+  
+  let currentTimeIs = hour + " : " + mint + " : " + sec + "  " + am_pm;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  document.querySelector("#digital-clock").innerHTML = currentTimeIs;
+}
+showTime();
